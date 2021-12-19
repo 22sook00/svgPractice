5c1a2cfd344efa6969c67ac65eeeafe8d9e15b5c
이숙영 <22sook00@gmail.com>
2021-12-19 13:54:21 +0900

    Initial setting eslint / prettier

--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 module.exports = {
-  reactStrictMode: true,
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  // images: {
+  //   domains: ["user-images.githubusercontent.com"],
+  //   loader: "imgix",
+  //   deviceSizes: [375, 700, 1200, 1920],
+  //   path: process.env.NEXT_PUBLIC_IMAGE_PATH,
+  // },
+  webpack(config) {
+    config.module.rules.push(
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      },
+      {
+        test: /\.md$/,
+        use: "raw-loader",
+      }
+    );
+
+    return config;
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
